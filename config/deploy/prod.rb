2e17611300ae371dde94964997a6b3297c722189
Tony Zanella <tony.zanella@gmail.com>
2018-03-21 15:09:14 -0700

    Make the runnable branch deployable

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -1,5 +1,6 @@
-server 'pres-robots1-prod.stanford.edu', user: 'pres', roles: %w[web app db monitor]
+server 'preservation-robots1-prod.stanford.edu', user: 'pres', roles: %w[web app db monitor]
 
 Capistrano::OneTimeKey.generate_one_time_key!
 
-set :deploy_environment, 'production'
+set :deploy_environment, 'prod'
+set :default_env, robot_environment: fetch(:deploy_environment)
