7af04f9948ddf3d76ed2d895385130da4ad0e99e
Sarav Shah <saravsshah@gmail.com>
2018-03-21 09:48:06 -0700

    Rspec tests for update_moab

---
+++ spec/preservation/update_moab_spec.rb
@@ -0,0 +1,37 @@
+describe Preservation::UpdateMoab do
+  let(:pres_update_moab) { described_class.new }
+  let(:full_druid) { 'druid:bj102hs9687' }
+  let(:mock_path) { instance_double(Pathname) }
+  let(:mock_so) { instance_double(Moab::StorageObject, object_pathname: mock_path) }
+  let(:mock_new_version) { instance_double(Moab::StorageObjectVersion) }
+  let(:verification_result) { instance_double(Moab::VerificationResult) }
+
+  context '#perform' do
+    before do
+      allow(Moab::StorageServices).to receive(:find_storage_object).and_return(mock_so)
+      allow(mock_path).to receive(:mkpath)
+    end
+
+    it 'calls #ingest_bag and verify_version_storage on Moab::StorageObjectVersion' do
+      allow(LyberCore::Log).to receive(:debug).with("update-moab druid:bj102hs9687 starting")
+      expect(Moab::StorageServices).to receive(:find_storage_object).with(full_druid, true).and_return(mock_so)
+      expect(mock_path).to receive(:mkpath)
+      expect(mock_so).to receive(:ingest_bag).and_return(mock_new_version)
+      allow(verification_result).to receive(:verified).and_return(true)
+      expect(mock_new_version).to receive(:verify_version_storage).and_return(verification_result)
+      pres_update_moab.perform(full_druid)
+    end
+
+    it 'raises ItemError when new version does not pass verification' do
+      full_druid_vers = "#{full_druid}-v0003"
+      error_message = "Failed verification for #{full_druid_vers}"
+      expect(mock_so).to receive(:ingest_bag).and_return(mock_new_version)
+      allow(verification_result).to receive(:verified).and_return(false)
+      expect(mock_new_version).to receive(:verify_version_storage).and_return(verification_result)
+      allow(verification_result).to receive(:to_json).and_return('some_json')
+      allow(verification_result).to receive(:entity).and_return(full_druid_vers)
+      expect(LyberCore::Log).to receive(:info).with('some_json')
+      expect { pres_update_moab.perform(full_druid) }.to raise_error(Preservation::ItemError, error_message)
+    end
+  end
+end
