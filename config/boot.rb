906f1817ae9e35c7e6894cb58c8d8b8021c152dc
Joe Atzberger <ohiocore@gmail.com>
2018-03-23 16:01:43 -0700

    Remove cruft

--- config/boot.rb
+++ config/boot.rb
@@ -1,5 +1,4 @@
 # Ensure subsequent requires search the correct local paths
-# $LOAD_PATH.unshift File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift File.expand_path(File.join(File.dirname(__FILE__), '..', 'robots'))
 
 require 'logger'
@@ -10,21 +9,10 @@ ROBOT_ROOT = File.expand_path(File.dirname(__FILE__) + '/..')
 ROBOT_LOG = Logger.new(File.join(ROBOT_ROOT, "log/#{environment}.log"))
 ROBOT_LOG.level = Logger::SEV_LABEL.index(ENV['ROBOT_LOG_LEVEL']) || Logger::INFO
 
-# Override Solrizer's logger before it gets a chance to load and pollute STDERR
-# begin
-#   require 'solrizer'
-#   Solrizer.logger = ROBOT_LOG
-# rescue LoadError, NameError, NoMethodError
-# end
-
 # Load core robot services
 require 'dor-services' # FIXME: do we really need dor-services?   Just for Dor::Config ??
 require 'lyber_core'
 LyberCore::Log.set_level(ROBOT_LOG.level)
-
-# Load any library files and all the robots
-# TODO: Maybe move auto-require to just run_robot and spec_helper?
-Dir["#{ROBOT_ROOT}/lib/*/*.rb"].each { |f| require f }
 require 'robots'
 
 # Load local environment configuration
