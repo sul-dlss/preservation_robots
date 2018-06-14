5e97a6ec10466e5ea082fa442417453292f57937
Tony Zanella <tony.zanella@gmail.com>
2018-06-14 11:20:21 -0700

    Migrate email addys to settings

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,6 @@
+# to let whenever access settings:
+require File.expand_path(File.dirname(__FILE__) + "/boot")
+
 every 5.minutes do
   # cannot use :output with Hash/String because we don't want append behavior
   set :output, (proc { '> log/verify.log 2> log/cron.log' })
@@ -13,15 +16,15 @@ end
 
 every :sunday, at: '5am' do
   set :output, nil
-  command "/bin/cat /var/log/preservation_robots/weekly_stats.log | mail -s 'Weekly preservation stats' sdr-discuss@lists.stanford.edu"
+  command "/bin/cat /var/log/preservation_robots/weekly_stats.log | mail -s 'Weekly preservation stats' #{Settings.email_addresses.discussion_list}"
 end
 
 every 1.day, at: '5am' do
   set :output, nil
-  rake 'preservation_errors', mailto: 'sdr2service@sul-sdr-services.stanford.edu'
+  rake 'preservation_errors', mailto: Settings.email_addresses.user_list
 end
 
 every 1.day, at: '5:10am' do
   set :output, nil
-  rake 'preservation_logs', mailto: 'sdr2service@sul-sdr-services.stanford.edu'
+  rake 'preservation_logs', mailto: Settings.email_addresses.user_list
 end
