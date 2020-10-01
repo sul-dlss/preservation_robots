d6a5839301d3a5d030b0cf260d845aee07c0a1e8
Johnathan Martin <john.martin@stanford.edu>
2020-10-01 16:03:53 -0700

    new test to confirm that all file and dir children of an updated moab are stat'ed after the deposit bag is removed

--- spec/preservation_ingest/complete_ingest_spec.rb
+++ spec/preservation_ingest/complete_ingest_spec.rb
@@ -1,9 +1,11 @@
 describe Robots::SdrRepo::PreservationIngest::CompleteIngest do
   let(:bare_druid) { 'jc837rq9922' }
   let(:druid) { "druid:#{bare_druid}" }
-  let(:deposit_dir_pathname) { Pathname(File.join(File.dirname(__FILE__), '..', 'fixtures', 'deposit', 'complete-ingest')) }
+  let(:spec_dir) { File.join(File.dirname(__FILE__), '..') }
+  let(:deposit_dir_pathname) { Pathname(File.join(spec_dir, 'fixtures', 'deposit', 'complete-ingest')) }
   let(:deposit_bag_pathname) { Pathname(File.join(deposit_dir_pathname, bare_druid)) }
-  let(:mock_storage_object) { instance_double(Moab::StorageObject, deposit_bag_pathname: deposit_bag_pathname) }
+  let(:mock_moab_pathname) { Pathname(File.realpath(File.join(spec_dir, 'fixtures', 'sdr2objects', 'bz', '514', 'sm', '9647', 'bz514sm9647'))) }
+  let(:mock_storage_object) { instance_double(Moab::StorageObject, deposit_bag_pathname: deposit_bag_pathname, object_pathname: mock_moab_pathname) }
   let(:mock_storage_objects) { [mock_storage_object] }
   let(:this_robot) { described_class.new }
 
@@ -49,6 +51,62 @@ describe Robots::SdrRepo::PreservationIngest::CompleteIngest do
               elapsed: 1,
               note: String)
       expect { this_robot.perform(druid) }.not_to raise_error
+    end
+
+    context 'attempts to stat the contents of the moab directory' do
+      let(:bare_druid) { 'bz514sm9647' }
+
+      let(:moab_file_and_dir_list) do
+        %w[
+          fixtures/sdr2objects/bz/514/sm/9647/bz514sm9647
+          fixtures/sdr2objects/bz/514/sm/9647/bz514sm9647/v0001
+          fixtures/sdr2objects/bz/514/sm/9647/bz514sm9647/v0001/data
+          fixtures/sdr2objects/bz/514/sm/9647/bz514sm9647/v0001/data/content
+          fixtures/sdr2objects/bz/514/sm/9647/bz514sm9647/v0001/data/content/SC1258_FUR_032a.jpg
+          fixtures/sdr2objects/bz/514/sm/9647/bz514sm9647/v0001/data/metadata
+          fixtures/sdr2objects/bz/514/sm/9647/bz514sm9647/v0001/data/metadata/contentMetadata.xml
+          fixtures/sdr2objects/bz/514/sm/9647/bz514sm9647/v0001/data/metadata/descMetadata.xml
+          fixtures/sdr2objects/bz/514/sm/9647/bz514sm9647/v0001/data/metadata/identityMetadata.xml
+          fixtures/sdr2objects/bz/514/sm/9647/bz514sm9647/v0001/data/metadata/provenanceMetadata.xml
+          fixtures/sdr2objects/bz/514/sm/9647/bz514sm9647/v0001/data/metadata/relationshipMetadata.xml
+          fixtures/sdr2objects/bz/514/sm/9647/bz514sm9647/v0001/data/metadata/rightsMetadata.xml
+          fixtures/sdr2objects/bz/514/sm/9647/bz514sm9647/v0001/data/metadata/technicalMetadata.xml
+          fixtures/sdr2objects/bz/514/sm/9647/bz514sm9647/v0001/data/metadata/versionMetadata.xml
+          fixtures/sdr2objects/bz/514/sm/9647/bz514sm9647/v0001/data/metadata/workflows.xml
+          fixtures/sdr2objects/bz/514/sm/9647/bz514sm9647/v0001/manifests
+          fixtures/sdr2objects/bz/514/sm/9647/bz514sm9647/v0001/manifests/fileInventoryDifference.xml
+          fixtures/sdr2objects/bz/514/sm/9647/bz514sm9647/v0001/manifests/manifestInventory.xml
+          fixtures/sdr2objects/bz/514/sm/9647/bz514sm9647/v0001/manifests/signatureCatalog.xml
+          fixtures/sdr2objects/bz/514/sm/9647/bz514sm9647/v0001/manifests/versionAdditions.xml
+          fixtures/sdr2objects/bz/514/sm/9647/bz514sm9647/v0001/manifests/versionInventory.xml
+        ]
+      end
+
+      it 'walks the moab directory and stats each file and dir contained within the moab' do
+        # using #ordered is typically to be avoided, but we want to make sure walking the moab happens
+        # after removing the deposit bag
+        expect(deposit_bag_pathname).to receive(:rmtree).ordered
+        expect(Find).to receive(:find).with(mock_moab_pathname).ordered.and_call_original
+
+        # File.realpath will return a string representing "the real (absolute) pathname of pathname in the actual filesystem
+        # not containing symlinks or useless dots" (see File class ruby docs).  this makes it easier to match the exact path
+        # without worrying about absolute path differences between different dev laptops or laptops and CI (while also allowing
+        # an exact match on path without just matching a substring).
+        moab_file_and_dir_list.each { |path_str| expect(File).to receive(:stat).with(File.realpath(File.join(spec_dir, path_str))) }
+        expect(this_robot.workflow_service).to receive(:update_status)
+        this_robot.perform(druid)
+      end
+
+      it 'raises an error if one of the files or directories in the moab is unreadable at the moment it is checked' do
+        v1_version_md_path_str = 'fixtures/sdr2objects/bz/514/sm/9647/bz514sm9647/v0001/data/metadata/versionMetadata.xml'
+        read_err_msg = 'Permission denied @ rb_sysopen'
+        exp_msg = /Error completing ingest for druid:bz514sm9647:.*#{read_err_msg}/
+
+        allow(File).to receive(:stat)
+        allow(File).to receive(:stat).with(File.realpath(File.join(spec_dir, v1_version_md_path_str))).and_raise(Errno::EACCES, read_err_msg)
+
+        expect { this_robot.perform(druid) }.to raise_error(Robots::SdrRepo::PreservationIngest::ItemError, a_string_matching(exp_msg))
+      end
     end
   end
 end
