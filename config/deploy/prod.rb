940aa0564958bc6a6bc0c6322cdec0766855ba7d
Tony Zanella <tony.zanella@gmail.com>
2021-10-18 15:12:16 -0700

    Deploy to robots2-prod

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -1,4 +1,6 @@
-server 'preservation-robots1-prod.stanford.edu', user: 'pres', roles: %w[web app db monitor]
+# while rebuilding robots1 as Ubuntu, robots2 picks up the slack
+# server 'preservation-robots1-prod.stanford.edu', user: 'pres', roles: %w[web app db monitor]
+server 'preservation-robots2-prod.stanford.edu', user: 'pres', roles: %w[web app db monitor]
 
 Capistrano::OneTimeKey.generate_one_time_key!
 
