fea065c46f25d4ff2687071eb3a2e44293cc0b49
Justin Coyne <jcoyne@justincoyne.com>
2021-10-18 15:30:40 -0500

    Move DepositBagValidator from Moab-versioning

--- lib/robots/sdr_repo/preservation_ingest/validate_bag.rb
+++ lib/robots/sdr_repo/preservation_ingest/validate_bag.rb
@@ -23,7 +23,7 @@ module Robots
 
         def validate_bag
           LyberCore::Log.debug("#{ROBOT_NAME} #{druid} starting")
-          deposit_bag_validator = Moab::DepositBagValidator.new(moab_object)
+          deposit_bag_validator = DepositBagValidator.new(moab_object)
           validation_errors = deposit_bag_validator.validation_errors
           raise(ItemError, "Bag validation failure(s) for #{druid}: #{validation_errors}") if validation_errors.any?
         end
