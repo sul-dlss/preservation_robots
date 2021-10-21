f682ceed5c35e85d4e24b290f6357bcf7b71edd4
Tony Zanella <tony.zanella@gmail.com>
2021-10-21 15:34:38 -0700

    robots2-qa/stage/prod are 'warm standbys'

--- config/deploy/stage.rb
+++ config/deploy/stage.rb
@@ -1,5 +1,6 @@
 server 'preservation-robots1-stage.stanford.edu', user: 'pres', roles: %w[web app db]
-server 'preservation-robots2-stage.stanford.edu', user: 'pres', roles: %w[web app db]
+# robots2-stage is a warm standby - no resque-pool up but ready to deploy to if needed
+# server 'preservation-robots2-stage.stanford.edu', user: 'pres', roles: %w[web app db]
 
 Capistrano::OneTimeKey.generate_one_time_key!
 
