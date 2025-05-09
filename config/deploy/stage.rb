4c9d2347688cf5dd85f080027f6ca4058b21919d
Michael J. Giarlo <mjgiarlo@stanford.edu>
2025-05-09 13:44:36 -0700

    Remove explicit one-time key setup

--- config/deploy/stage.rb
+++ config/deploy/stage.rb
@@ -4,7 +4,5 @@ server 'preservation-robots1-stage.stanford.edu', user: 'pres', roles: %w[web ap
 # robots2-stage is a warm standby - no sidekiq up but ready to deploy to if needed
 # server 'preservation-robots2-stage.stanford.edu', user: 'pres', roles: %w[web app db worker]
 
-Capistrano::OneTimeKey.generate_one_time_key!
-
 set :deploy_environment, 'stage'
 set :default_env, robot_environment: fetch(:deploy_environment)
